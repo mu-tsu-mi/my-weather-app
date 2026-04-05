4f49ec7cefb249d88819865d80ffb153521e95df
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2026-04-05 18:30:27 +1000

    Add fetchData, WIP current weather component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import { fetchWeatherData } from "./services/open-meteo-api";
 import type { WeatherData } from "./services/open-meteo-api";
+import CurrentWeatherCard from "./components/current-weather";
 
-// Update from import/export to UseEffect(then render with useState) or function
 function App() {
+  const [weather, setWeather] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetchWeatherData();
+        setWeather(data);
+        setError(null);
+      } catch (error) {
+        setError(error instanceof Error ? error.message : String(error));
+        setWeather(null);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Error is occuring: {error}</h2>;
+  if (!weather) return <h2>Sorry, weather data is not available</h2>;
+
   return (
     <>
       <section id="act"></section>
+      <CurrentWeatherCard weather={weather} />
 
       <section id="fcst"></section>
     </>
---
+++ src/components/current-weather.tsx
@@ -0,0 +1,9 @@
+import type { WeatherData } from "../services/open-meteo-api";
+
+export default function CurrentWeatherCard({
+  weather,
+}: {
+  weather: WeatherData;
+}) {
+  return <>current weather</>;
+}
