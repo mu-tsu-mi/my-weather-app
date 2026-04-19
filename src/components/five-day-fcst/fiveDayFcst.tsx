d1d5e0e5729e5f60abecd2c7be482b5aa35bd36f
Mutsumi Ishihara <mutts.m.mutts@gmail.com>
2026-04-19 11:58:11 +1000

    apply context - FiveDayFcstCard

--- src/components/five-day-fcst/fiveDayFcst.tsx
+++ src/components/five-day-fcst/fiveDayFcst.tsx
@@ -1,3 +1,22 @@
+import { useWeather } from "../../context/WeatherContext";
+
 export default function FiveDayFcstCard() {
+  const weatherContext = useWeather();
+  if (!weatherContext) return null;
+  const { weather, loading, error } = weatherContext;
+  if (!weather) return null;
+  if (loading) return <h2>Loading...</h2>;
+  if (error) return <h2>Error is occuring: {error}</h2>;
+
+  const { daily } = weather;
+  const {
+    temperature_2m_max,
+    temperature_2m_min,
+    time,
+    uv_index_max,
+    weather_code,
+    wind_speed_10m_max,
+  } = daily;
+
   return <div>fcst</div>;
 }
